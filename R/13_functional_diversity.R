3b4be3bfa796858e2d9c68a6cce44adb038e51d6
benjaminfigueiredo <benjaminfigueiredo@outlook.com>
2022-08-18 19:34:29 -0300

    Added a script calculating functional diversity metrics in the cestes database. In the class we also did some phyloginy things that I don't know how to reproduce.

---
+++ R/13_functional_diversity.R
@@ -0,0 +1,43 @@
+library(vegan)
+
+comm <- read.csv("data/raw/cestes/comm.csv")
+traits <- read.csv("data/raw/cestes/traits.csv")
+
+rownames(comm) <- paste0("Site", comm[,1])
+comm <- comm[,-1]
+
+rownames(traits) <- traits$Sp
+traits <- traits[,-1]
+
+richness <- vegan::specnumber(comm)
+richness
+
+# the standard entropy-based diversity metrics
+
+shannon <- vegan::diversity(comm)
+shannon
+simpson <- vegan::diversity(comm, index = "simpson")
+simpson
+
+# now we go to functional traits
+
+gow <- cluster::daisy(traits, metric = "gower")
+gow # the gower metric measures the distance in trait space among many different data types
+gow2 <- FD::gowdis(traits)
+#implementations in R vary and the literature reports extensions and modifications
+identical(gow, gow2) #not the same but why?
+plot(gow, gow2, asp = 1) # not the same class, but are identical as we can see in plot
+
+# now we can calculate rao's quadratic entropy
+library(SYNCSA)
+tax <- rao.diversity(comm)
+fun <- rao.diversity(comm, traits = traits)
+tax
+plot(fun$Simpson,fun$FunRao, pch = 19, asp = 1)
+abline(a = 0, b = 1) # identity line to situate the points
+
+library(FD)
+FuncDiv1 <- dbFD(x = gow, a = comm, messages = F)
+names(FuncDiv1)
+FuncDiv <- dbFD(x = traits, a = comm, messages = F)
+FuncDiv # here we see a whole lot of metrics of functional diversity, inlcude the rao thing above
